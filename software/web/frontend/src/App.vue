26a9c1070f9ad4385a20e2c44993e9ff7de4a2a7
Shawn Duong <me@shawnd.xyz>
2024-01-24 22:53:26 -0800

    Created login page UI.

--- software/web/frontend/src/App.vue
+++ software/web/frontend/src/App.vue
@@ -1,30 +1,47 @@
+<!-- Template -->
+
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+
+<router-view />
+
 </template>
 
+<!-- Style -->
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+
+:root {
+	--dark  : #575757;
+	--light : #D9D9D9;
+	--white : #FFFFFF;
+	--black : #000000;
 }
 
-nav {
-  padding: 30px;
+@font-face {
+	font-family: "Montserrat";
+	src: url("@/assets/fonts/Montserrat/Montserrat-Regular.ttf");
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+html {
+	background-color: var(--dark);
+	color: var(--white);
+	font-family: "Montserrat";
 }
 
-nav a.router-link-exact-active {
-  color: #42b983;
+body {
+	margin: 0;
 }
+
+h1 {
+	font-size: 64px;
+	font-weight: 400;
+	margin-bottom: 0.1em;
+}
+
+h2 {
+	font-size: 20px;
+	font-weight: 200;
+	margin-bottom: 0.1em;
+}
+
 </style>
