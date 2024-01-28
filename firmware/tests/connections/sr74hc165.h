8f9eef4519ebf82709349da705bf8e2b036656a4
shawnduong <me@shawnd.xyz>
2024-01-27 18:05:13 -0800

    Made some connect test definitions private.

--- firmware/tests/connections/sr74hc165.h
+++ firmware/tests/connections/sr74hc165.h
@@ -1,33 +1,33 @@
 #pragma once
 
-enum SR_74HC165_PINOUT
+enum _SR_74HC165_PINOUT
 {
-	SR_74HC165_PL = 0,
-	SR_74HC165_CP = 14,
-	SR_74HC165_Q7 = 12,
-	SR_74HC165_CE = 15
+	_SR_74HC165_PL = 0,
+	_SR_74HC165_CP = 14,
+	_SR_74HC165_Q7 = 12,
+	_SR_74HC165_CE = 15
 };
 
 void sr_74hc165_init()
 {
-	pinMode(SR_74HC165_PL, OUTPUT);
-	pinMode(SR_74HC165_CP, OUTPUT);
-	pinMode(SR_74HC165_Q7, INPUT);
-	pinMode(SR_74HC165_CE, OUTPUT);
+	pinMode(_SR_74HC165_PL, OUTPUT);
+	pinMode(_SR_74HC165_CP, OUTPUT);
+	pinMode(_SR_74HC165_Q7, INPUT);
+	pinMode(_SR_74HC165_CE, OUTPUT);
 }
 
 char sr_74hc165_read()
 {
-	digitalWrite(SR_74HC165_PL, LOW);
+	digitalWrite(_SR_74HC165_PL, LOW);
 	delayMicroseconds(10);
-	digitalWrite(SR_74HC165_PL, HIGH);
+	digitalWrite(_SR_74HC165_PL, HIGH);
 	delayMicroseconds(10);
 
-	digitalWrite(SR_74HC165_CP, HIGH);
+	digitalWrite(_SR_74HC165_CP, HIGH);
 	delayMicroseconds(10);
-	digitalWrite(SR_74HC165_CE, LOW);
-	char data = shiftIn(SR_74HC165_Q7, SR_74HC165_CP, LSBFIRST);
-	digitalWrite(SR_74HC165_CE, HIGH);
+	digitalWrite(_SR_74HC165_CE, LOW);
+	char data = shiftIn(_SR_74HC165_Q7, _SR_74HC165_CP, LSBFIRST);
+	digitalWrite(_SR_74HC165_CE, HIGH);
 
 	return data;
 }
