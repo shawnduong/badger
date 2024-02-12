5551d07bb726c2d2398815246f0b01cde69ba4f1
shawnduong <me@shawnd.xyz>
2024-02-12 00:17:36 -0800

    Ported status block code to the ATmega328-P.

--- firmware/tests/status_block/status_block.ino
+++ firmware/tests/status_block/status_block.ino
@@ -9,11 +9,11 @@
  * BUZZ  <-> D1
  */
 
-#define SER   13
-#define RCLK  15
-#define SRCLK 14
+#define SCLK 13  // D13 | Pin 19: PB5
+#define MOSI 11  // D11 | Pin 17: PB3
+#define CS1   9  // D9  | Pin 15: PB1
 
-#define BUZZ  5
+#define TONE 14  // A0  | Pin 23: PC0
 
 byte tests[] = {
 	0b00000000,  // Reset
@@ -32,31 +32,48 @@ byte tests[] = {
 	0b10010111,  // Green, Red, Buzzer
 };
 
+/* On the Uno, some SRs (especially clones) are too slow, so we need to slow
+   down the rate at which data is shifted out. */
+void _shiftOut(uint8_t outPin, uint8_t clkPin, uint8_t order, byte data)
+{
+	for (uint8_t i = 0; i < 8; i++)
+	{
+		delayMicroseconds(250);
+		digitalWrite(clkPin, LOW);
+		delayMicroseconds(250);
+		digitalWrite(outPin, (data >> i) & 1);
+		delayMicroseconds(250);
+		digitalWrite(clkPin, HIGH);
+	}
+}
+
 void setup()
 {
 	Serial.begin(9600);
 
-	pinMode(SER  , OUTPUT);
-	pinMode(RCLK , OUTPUT);
-	pinMode(SRCLK, OUTPUT);
-	pinMode(BUZZ , OUTPUT);
+	pinMode(MOSI, OUTPUT);
+	pinMode(CS1 , OUTPUT);
+	pinMode(SCLK, OUTPUT);
+	pinMode(TONE, OUTPUT);
+
+	digitalWrite(CS1, HIGH);
 }
 
 void loop()
 {
 	for (uint8_t i = 0; i < sizeof(tests); i++)
 	{
-		digitalWrite(RCLK, LOW);
-		shiftOut(SER, SRCLK, LSBFIRST, tests[i]);
-		digitalWrite(RCLK, HIGH);
-		delay(5000);
+		digitalWrite(CS1, LOW);
+		_shiftOut(MOSI, SCLK, LSBFIRST, tests[i]);
+		digitalWrite(CS1, HIGH);
+		delay(2000);
 	}
 
-	tone(BUZZ, 513);
+	tone(TONE, 513);
 	delay(200);
-	tone(BUZZ, 1046);
+	tone(TONE, 1046);
 	delay(300);
 
-	noTone(BUZZ);
+	noTone(TONE);
 	delay(1500);
 }
