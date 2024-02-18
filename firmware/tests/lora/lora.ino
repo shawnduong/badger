60293ca2dd729ba175622b3dd0c28b530fec68b7
shawnduong <me@shawnd.xyz>
2024-02-18 03:18:56 -0800

    Added some basic code for a LoRa test.

---
+++ firmware/tests/lora/lora.ino
@@ -0,0 +1,64 @@
+/* LoRa
+ * See b1/LoRa schematic for more details.
+ * Uses the MCP23017's GPA6 as NSS, GPA7 as RESET, and GPB1 as DIO0.
+ */
+
+#include <LoRa.h>
+#include <MCP23017.h>  // MCP23017 by Bertrand Lemasle
+
+#define LORA_NSS   6  // MCP23017
+#define LORA_RST   7  // MCP23017
+#define LORA_DIO0  9  // MCP23017
+
+#define MCP23017_I2C_ADDRESS 0x20
+MCP23017 mcp23017 = MCP23017(MCP23017_I2C_ADDRESS);
+
+void setup()
+{
+	Serial.begin(9600);
+	Serial.println("LoRa Test");
+
+	Wire.begin();
+	mcp23017.init();
+
+	mcp23017.writeRegister(MCP23017Register::GPIO_A, 0x00);
+	mcp23017.writeRegister(MCP23017Register::GPIO_B, 0x00);
+
+//	LoRa.setPins(&mcp23017, LORA_NSS, LORA_RST, LORA_DIO0);
+	LoRa.setPins(LORA_NSS, LORA_RST, LORA_DIO0);
+	LoRa.begin(433e6);
+}
+
+/* LoRa Tx test. */
+void test_lora_tx()
+{
+	Serial.println("Sending packet... ");
+	LoRa.beginPacket();
+	LoRa.println(F("Hello world!"));
+	LoRa.endPacket();
+	Serial.println("done. ");
+}
+
+/* LoRa Rx test. */
+void test_lora_rx()
+{
+	while (!LoRa.parsePacket())  delay(100);
+
+	Serial.print("Received packet '");
+	while (LoRa.available())  Serial.print((char)LoRa.read());
+	Serial.print("' with RSSI ");
+	Serial.println(LoRa.packetRssi());
+}
+
+void loop()
+{
+	Serial.println("\nStarting a test iteration.\n");
+	#ifdef TEST_TX
+		test_lora_tx();
+	#endif
+	#ifdef TEST_RX
+		test_lora_rx();
+	#endif
+	Serial.println("Test iteration complete.\n");
+	delay(2000);
+}
