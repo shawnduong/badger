7d8cc42d6d9879fa9e0793f77a155f055e0100b0
Shawn Duong <me@shawnd.xyz>
2024-03-01 02:22:03 -0800

    Modified naming conventions in some test code.

--- software/platform/main/test/test_00_accounts.py
+++ software/platform/main/test/test_00_accounts.py
@@ -1,19 +1,19 @@
 import json
 import requests
 
-endpoint = "http://localhost:8080"
-api = endpoint+"/api/v1"
+ENDPOINT = "http://localhost:8080"
+API = ENDPOINT+"/api/v1"
 
 r1 = requests.Session()
-r1.post(endpoint+"/login", data={"uid": 0xfeedf00d, "password": "admin"})
+r1.post(ENDPOINT+"/login", data={"uid": 0xfeedf00d, "password": "admin"})
 
 r2 = requests.Session()
-r2.post(endpoint+"/login", data={"uid": 0xf00df00d, "password": "user"})
+r2.post(ENDPOINT+"/login", data={"uid": 0xf00df00d, "password": "user"})
 
 # --[ MAKE AN ACCOUNT ]--
 
 def test_admin_user_post_0():
-	r = r1.post(api+"/admin/user", json={
+	r = r1.post(API+"/admin/user", json={
 		"uid": 0xdeadbeef,
 		"points": None,
 		"claimed": False,
@@ -23,7 +23,7 @@ def test_admin_user_post_0():
 	assert r.status_code == 400
 
 def test_admin_user_post_1():
-	r = r1.post(api+"/admin/user", json={
+	r = r1.post(API+"/admin/user", json={
 		"uid": 0xdeadbeef,
 		"email": None,
 		"points": None,
@@ -34,7 +34,7 @@ def test_admin_user_post_1():
 	assert r.status_code == 201
 
 def test_admin_user_post_2():
-	r = r1.post(api+"/admin/user", json={
+	r = r1.post(API+"/admin/user", json={
 		"uid": 0xdeadbeef,
 		"email": None,
 		"points": None,
@@ -45,7 +45,7 @@ def test_admin_user_post_2():
 	assert r.status_code == 409
 
 def test_admin_user_post_3():
-	r = r2.post(api+"/admin/user", json={
+	r = r2.post(API+"/admin/user", json={
 		"uid": 0xdeadbeef+1,
 		"email": None,
 		"points": None,
@@ -58,7 +58,7 @@ def test_admin_user_post_3():
 # --[ CLAIM AN ACCOUNT ]--
 
 def test_user_user_post_0():
-	r = requests.post(api+"/user/user", data={
+	r = requests.post(API+"/user/user", data={
 		"uid": 0xdeadbeef,
 		"password": "hunter2",
 		"custom": "",
@@ -66,7 +66,7 @@ def test_user_user_post_0():
 	assert r.status_code == 400
 
 def test_user_user_post_1():
-	r = requests.post(api+"/user/user", data={
+	r = requests.post(API+"/user/user", data={
 		"uid": 0xdeadbeef,
 		"email": "jdoe@email.com",
 		"password": "hunter2",
@@ -75,7 +75,7 @@ def test_user_user_post_1():
 	assert r.status_code == 201
 
 def test_user_user_post_2():
-	r = requests.post(api+"/user/user", data={
+	r = requests.post(API+"/user/user", data={
 		"uid": 0xdeadbeef,
 		"email": "jdoe@email.com",
 		"password": "hunter2",
@@ -84,7 +84,7 @@ def test_user_user_post_2():
 	assert r.status_code == 409
 
 def test_user_user_post_3():
-	r = requests.post(api+"/user/user", data={
+	r = requests.post(API+"/user/user", data={
 		"uid": 9999,
 		"email": "jdoe@email.com",
 		"password": "hunter2",
@@ -95,7 +95,7 @@ def test_user_user_post_3():
 # --[ GET INFO ABOUT YOUR OWN ACCOUNT ]--
 
 def test_user_user_get_0():
-	r = r2.get(api+"/user/user")
+	r = r2.get(API+"/user/user")
 	assert r.status_code == 200
 
 	data = json.loads(r.content)
@@ -108,7 +108,7 @@ def test_user_user_get_0():
 # --[ UPDATE INFO ABOUT YOUR OWN ACCOUNT ]--
 
 def test_user_user_patch_0():
-	r = r2.patch(api+"/user/user", json={
+	r = r2.patch(API+"/user/user", json={
 		"uid": 0xf00df00d,
 		"email": "test@example.com",
 		"password": "********",
@@ -116,25 +116,25 @@ def test_user_user_patch_0():
 	})
 	assert r.status_code == 201
 
-	r = r2.get(api+"/user/user")
+	r = r2.get(API+"/user/user")
 	data = json.loads(r.content)
 	assert data["email"] == "test@example.com"
 
 # --[ GET INFO ABOUT ALL ACCOUNTS ]--
 
 def test_admin_get_user_0():
-	r = r1.get(api+"/admin/user")
+	r = r1.get(API+"/admin/user")
 	data = json.loads(r.content)
 	assert r.status_code == 200 and len(data) > 0
 
 def test_admin_get_user_1():
-	r = r2.get(api+"/admin/user")
+	r = r2.get(API+"/admin/user")
 	assert r.status_code == 401
 
 # --[ EDIT INFO ABOUT AN ACCOUNT ]--
 
 def test_admin_patch_user_0():
-	r = r1.patch(api+"/admin/user/3", json={
+	r = r1.patch(API+"/admin/user/3", json={
 		"uid": 0xdeadbeef,
 		"points": 100,
 		"claimed": True,
@@ -144,14 +144,14 @@ def test_admin_patch_user_0():
 	assert r.status_code == 400
 
 	s = requests.Session()
-	r = s.post(endpoint+"/login", data={"uid": 0xdeadbeef, "password": "hunter2"})
+	r = s.post(ENDPOINT+"/login", data={"uid": 0xdeadbeef, "password": "hunter2"})
 	assert r.status_code == 200
-	r = s.get(api+"/user/user")
+	r = s.get(API+"/user/user")
 	data = json.loads(r.content)
 	assert data["points"] == None
 
 def test_admin_patch_user_1():
-	r = r1.patch(api+"/admin/user/3", json={
+	r = r1.patch(API+"/admin/user/3", json={
 		"uid": 0xdeadbeef,
 		"email": "jdoe@email.com",
 		"points": 100,
@@ -162,14 +162,14 @@ def test_admin_patch_user_1():
 	assert r.status_code == 201
 
 	s = requests.Session()
-	r = s.post(endpoint+"/login", data={"uid": 0xdeadbeef, "password": "hunter2"})
+	r = s.post(ENDPOINT+"/login", data={"uid": 0xdeadbeef, "password": "hunter2"})
 	assert r.status_code == 200
-	r = s.get(api+"/user/user")
+	r = s.get(API+"/user/user")
 	data = json.loads(r.content)
 	assert data["points"] == 100
 
 def test_admin_patch_user_2():
-	r = r2.patch(api+"/admin/user/3", json={
+	r = r2.patch(API+"/admin/user/3", json={
 		"uid": 0xdeadbeef,
 		"email": "jdoe@email.com",
 		"points": 999,
@@ -182,14 +182,14 @@ def test_admin_patch_user_2():
 # --[ DELETE AN ACCOUNT ]--
 
 def test_admin_user_delete_0():
-	r = r1.delete(api+"/admin/user/3")
+	r = r1.delete(API+"/admin/user/3")
 	assert r.status_code == 200
 
 def test_admin_user_delete_1():
-	r = r1.delete(api+"/admin/user/3")
+	r = r1.delete(API+"/admin/user/3")
 	assert r.status_code == 404
 
 def test_admin_user_delete_2():
-	r = r2.delete(api+"/admin/user/3")
+	r = r2.delete(API+"/admin/user/3")
 	assert r.status_code == 401
 
