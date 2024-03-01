8a5c31c6a7a9a3fd82b7f5258e5f8caec33a9605
Shawn Duong <me@shawnd.xyz>
2024-03-01 02:36:47 -0800

    Modified naming convention for Public API.

--- software/platform/services/public/public.py
+++ software/platform/services/public/public.py
@@ -3,6 +3,8 @@ from app import *
 import json
 import re
 
+API = "/api/v1/public"
+
 def _valid_email(email: str):
 	"""
 	Return true if a possible email address string is valid. Credits to
@@ -12,7 +14,7 @@ def _valid_email(email: str):
 	pattern = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])"
 	return re.match(pattern, email)
 
-@app.route("/api/v1/public/reset", methods=["POST"])
+@app.route(API+"/reset", methods=["POST"])
 @failsafe_500
 def v1_post():
 	data = json.loads(request.json)
