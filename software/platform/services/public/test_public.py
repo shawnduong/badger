dc86a2a43c853d90b7d40c13c2830bd6a2a873ba
Shawn Duong <me@shawnd.xyz>
2024-03-01 02:20:33 -0800

    Restructured test code.

--- software/platform/services/public/test_public.py
+++ software/platform/services/public/test_public.py
@@ -1,21 +1,23 @@
 import requests
 
+API = "http://localhost:64094/api/v1/public"
+
 def test_reset_post_0():
-	r = requests.post("http://localhost:64094/api/v1/public/reset", json='{"email": "bar@email.com"}')
+	r = requests.post(API+"/reset", json='{"email": "bar@email.com"}')
 	assert r.status_code == 202
 
 def test_reset_post_1():
-	r = requests.post("http://localhost:64094/api/v1/public/reset", json='{"email": "bar@emailcom"}')
+	r = requests.post(API+"/reset", json='{"email": "bar@emailcom"}')
 	assert r.status_code == 400
 
 def test_reset_post_2():
-	r = requests.post("http://localhost:64094/api/v1/public/reset", json='{"email": "baremail.com"}')
+	r = requests.post(API+"/reset", json='{"email": "baremail.com"}')
 	assert r.status_code == 400
 
 def test_reset_post_3():
-	r = requests.post("http://localhost:64094/api/v1/public/reset", json='{"email": "baremailcom"}')
+	r = requests.post(API+"/reset", json='{"email": "baremailcom"}')
 	assert r.status_code == 400
 
 def test_reset_post_4():
-	r = requests.post("http://localhost:64094/api/v1/public/reset", json='{"emil": "bar@email.com"}')
+	r = requests.post(API+"/reset", json='{"emil": "bar@email.com"}')
 	assert r.status_code == 400
