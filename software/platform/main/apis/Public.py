438bd3d5c551f18757186c950dc90aa9f73bfe1e
shawnduong <me@shawnd.xyz>
2024-02-29 23:10:57 -0800

    Renamed method in Public.

--- software/platform/main/apis/Public.py
+++ software/platform/main/apis/Public.py
@@ -7,7 +7,7 @@ from flask_login import current_user, login_required
 publicPrefix = "/api/v1/public"
 
 @app.route(publicPrefix+"/reset", methods=["POST"])
-def reset_post():
+def public_reset_post():
 	# Stub.
 	r = requests.post(IMPLEMENTATION["public"]+"/reset", json=request.json)
 	return r.content, r.status_code
