616037b3b8493fce3d71690d648a799592d156e8
shawnduong <me@shawnd.xyz>
2024-03-02 16:24:13 -0800

    Broke out manage into modules.

--- software/platform/main/api/manage/submission.py
+++ software/platform/main/api/manage/submission.py
@@ -5,41 +5,6 @@ import requests
 
 managePrefix = "/api/v1/manage"
 
-@app.route(managePrefix+"/code", methods=["GET"])
-@admin_required
-@failsafe_500
-def manage_code_get():
-	r = requests.get(IMPLEMENTATION["manage"]+"/code")
-	return r.content, r.status_code
-
-@app.route(managePrefix+"/code", methods=["POST"])
-@admin_required
-@failsafe_500
-def manage_code_post():
-	r = requests.post(IMPLEMENTATION["manage"]+"/code", json=request.json)
-	return r.content, r.status_code
-
-@app.route(managePrefix+"/code/<codeId>", methods=["GET"])
-@admin_required
-@failsafe_500
-def manage_code_get_specific(codeId: int):
-	r = requests.get(IMPLEMENTATION["manage"]+f"/code/{codeId}")
-	return r.content, r.status_code
-
-@app.route(managePrefix+"/code/<codeId>", methods=["PATCH"])
-@admin_required
-@failsafe_500
-def manage_code_patch(codeId: int):
-	r = requests.patch(IMPLEMENTATION["manage"]+f"/code/{codeId}", json=request.json)
-	return r.content, r.status_code
-
-@app.route(managePrefix+"/code/<codeId>", methods=["DELETE"])
-@admin_required
-@failsafe_500
-def manage_code_delete(codeId: int):
-	r = requests.delete(IMPLEMENTATION["manage"]+f"/code/{codeId}")
-	return r.content, r.status_code
-
 @app.route(managePrefix+"/submission", methods=["GET"])
 @admin_required
 @failsafe_500
