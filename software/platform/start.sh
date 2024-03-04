d1c0654a52ea1617b0da03a22520da18c096156b
Shawn Duong <me@shawnd.xyz>
2024-03-04 02:53:05 -0800

    Created scripts to orchestrate setup.

---
+++ software/platform/start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# You must have all the dependencies installed or have activated a venv with the
+# dependencies installed before running this script.
+
+trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
+
+workdir=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
+
+# Services.
+${workdir}/services/admin/main.py &
+${workdir}/services/manage/main.py &
+${workdir}/services/public/main.py &
+${workdir}/services/user/main.py &
+
+# Primary platform.
+${workdir}/main/main.py
---
+++ software/platform/test.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# You must have all the dependencies installed or have created a venv with the
+# dependencies installed before running this script.
+
+echo "WARNING: THIS WILL DELETE ANY EXISTING INSTANCES AND THEIR DATA."
+read -p "Press ENTER to acknowledge and proceed." </dev/tty
+
+workdir=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
+
+pkill -f ${workdir}/services/admin/main.py
+pkill -f ${workdir}/services/manage/main.py
+pkill -f ${workdir}/services/public/main.py
+pkill -f ${workdir}/services/user/main.py
+pkill -f ${workdir}/main/main.py
+
+tmux new-session \; \
+	split-window -v \; \
+	attach \; \
+	select-pane -t 0 \; \
+	send-keys -t 0 \
+		"
+		trap 'trap - SIGTERM && kill -- -$$' SIGINT SIGTERM EXIT
+		source ${workdir}/env/bin/activate
+		rm -f ${workdir}/services/*/instance/*.sqlite
+		rm -f ${workdir}/services/*/*.sqlite
+		rm -f ${workdir}/main/instance/*.sqlite
+		rm -f ${workdir}/main/*.sqlite
+		${workdir}/services/admin/main.py &
+		${workdir}/services/manage/main.py &
+		${workdir}/services/public/main.py &
+		${workdir}/services/user/main.py &
+		${workdir}/main/main.py &
+		" \; \
+	send-keys -t 1 \
+		"
+		source ${workdir}/env/bin/activate
+		sleep 3
+		pytest
+		";
+
